3652e08435b11221cd1fc22139702ce8025908c3
takasehideki <takase@i.kyoto-u.ac.jp>
2019-11-27 17:45:12 +0900

    mix nerves.new nerves_pocket_beagle --target bbb

---
+++ lib/nerves_pocket_beagle.ex
@@ -0,0 +1,18 @@
+defmodule NervesPocketBeagle do
+  @moduledoc """
+  Documentation for NervesPocketBeagle.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesPocketBeagle.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nerves_pocket_beagle_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesPocketBeagleTest do
+  use ExUnit.Case
+  doctest NervesPocketBeagle
+
+  test "greets the world" do
+    assert NervesPocketBeagle.hello() == :world
+  end
+end
